3114fc9d1609f6a01582a4c4148cb9596b620975
Gustav Sörnäs <gustav@sornas.net>
2021-02-19 19:20:03 +0100

    demo automatic linkage (NOT WORKING)

--- src/main.rs
+++ src/main.rs
@@ -47,6 +47,25 @@ fn parse_args() -> Args {
     args
 }
 
+#[sylt_macro::extern_link]
+pub fn f(x: sylt::Value, _typecheck: bool) -> Result<sylt::Value, sylt::error::ErrorKind> {
+    Ok(x)
+}
+
+#[sylt_macro::extern_link(g)]
+pub fn f2(x: sylt::Value, _typecheck: bool) -> Result<sylt::Value, sylt::error::ErrorKind> {
+    Ok(x)
+}
+
+mod m1 {
+    mod m2 {
+        #[sylt_macro::extern_link(h)]
+        pub fn f2(x: sylt::Value, _typecheck: bool) -> Result<sylt::Value, sylt::error::ErrorKind> {
+            Ok(x)
+        }
+    }
+}
+
 sylt_macro::extern_function!(
     extern_test
     [sylt::Value::Float(x), sylt::Value::Float(y)] -> sylt::Type::Float => {
