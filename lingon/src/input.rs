21080ce59e3d244638b088c8408721b03a4a7817
Edvard Thörnros <edvard.thornros@gmail.com>
2021-04-03 23:53:37 +0200

    document the input!

--- lingon/src/input.rs
+++ lingon/src/input.rs
@@ -1,14 +1,39 @@
+//! The easiest way to get the use to make decisions.
+//!
+//! A small example:
+//! ```ignore
+//! fn main() {
+//!     let input = input::InputManager::new(sdl);
+//!     input.bind(input::Name::Left, input::Device::Key(input::KeycodeA));
+//!     loop {
+//!         /// ... In main loop
+//!         if input.pressed(input::Name::Left) {
+//!             player_jump();
+//!         }
+//!     }
+//! }
+//! ```
+//! Here we bind the "A" key to the input "Left", and tells the plyaer to jump when the
+//! button is first pressed.
+//!
+//! For this to work the InputManager needs to be passed around everywhere it's used,
+//! and to use the rumble feature it needs to be mutable.
+
 use luminance_sdl2::sdl2;
 use sdl2::event::{Event, WindowEvent};
 use std::collections::HashMap;
 
-pub use sdl2::{GameControllerSubsystem, Sdl};
-pub use sdl2::controller::{Axis, Button, GameController};
+use sdl2::{GameControllerSubsystem, Sdl};
+use sdl2::controller::GameController;
+pub use sdl2::controller::{Axis, Button};
 pub use sdl2::keyboard::Keycode;
 pub use sdl2::mouse::MouseButton;
 
+/// All the different kinds of input device we can listen
+/// for.
 #[derive(Hash, Copy, Clone, Debug, Eq, PartialEq)]
 pub enum Device {
+    /// The magic quit event, when the window is closed.
     Quit,
     Key(Keycode),
     Mouse(MouseButton),
@@ -16,6 +41,7 @@ pub enum Device {
     Axis(u32, Axis),
 }
 
+/// A list of all valid inputs, you are expected to change this.
 #[derive(Hash, Copy, Clone, Debug, Eq, PartialEq)]
 pub enum Name {
     Left,
@@ -26,12 +52,13 @@ pub enum Name {
 }
 
 #[derive(Copy, Clone, Debug)]
-pub enum KeyState {
+enum KeyState {
     Down(usize),
     Up(usize),
     Analog(f32),
 }
 
+/// The one stop shop for all things input!
 pub struct InputManager {
     frame: usize,
     controllers: GameControllerSubsystem,
@@ -40,12 +67,14 @@ pub struct InputManager {
     opened_controllers: HashMap<u32, GameController>,
 }
 
+/// [i32::MIN, i32::MAX] -> [-1.0, 1.0)
 fn remap(value: i16) -> f32 {
     // MIN has a larger absolute value,
     // this garantees that the values in [-1, 1).
     -(value as f32) / (i16::MIN as f32)
 }
 
+/// When an analog signal becomes digital.
 const TRIGGER_LIMIT: f32 = 0.1;
 
 impl InputManager {
@@ -62,12 +91,14 @@ impl InputManager {
         }
     }
 
+    /// Craetes a new binding to listen to.
     pub fn bind(&mut self, device: Device, name: Name) {
         self.physical_inputs.insert(device, name);
         self.virtual_inputs.insert(name, KeyState::Up(0));
     }
 
     #[allow(dead_code)]
+    /// Check if the input is down this frame.
     pub fn down(&self, name: Name) -> bool {
         match self.virtual_inputs.get(&name) {
             Some(KeyState::Down(_)) => true,
@@ -81,6 +112,7 @@ impl InputManager {
     }
 
     #[allow(dead_code)]
+    /// Check if the input is inactive.
     pub fn up(&self, name: Name) -> bool {
         match self.virtual_inputs.get(&name) {
             Some(KeyState::Down(_)) => false,
@@ -93,6 +125,7 @@ impl InputManager {
         }
     }
 
+    /// Check if the input is pressed this frame.
     pub fn pressed(&self, name: Name) -> bool {
         match self.virtual_inputs.get(&name) {
             Some(KeyState::Down(frame)) => self.frame == *frame,
@@ -104,6 +137,7 @@ impl InputManager {
     }
 
     #[allow(dead_code)]
+    /// Check if the input was released this frame.
     pub fn released(&self, name: Name) -> bool {
         match self.virtual_inputs.get(&name) {
             Some(KeyState::Up(frame)) => self.frame == *frame,
@@ -114,6 +148,7 @@ impl InputManager {
         }
     }
 
+    /// Returns the inputs as analog signals.
     pub fn value(&self, name: Name) -> f32 {
         match self.virtual_inputs.get(&name) {
             Some(KeyState::Up(_)) => 0.0,
@@ -126,6 +161,7 @@ impl InputManager {
         }
     }
 
+    /// Shake that controller!
     pub fn rumble(&mut self, controller: u32, lo: f32, hi: f32, time: f32) -> Result<(), ()> {
         if let Some(controller) = self.opened_controllers.get_mut(&controller) {
             let lo = (lo * (u16::MAX as f32)) as u16;
@@ -137,6 +173,7 @@ impl InputManager {
         }
     }
 
+    /// Update the state of the input.
     pub fn poll(&mut self, sdl: &sdl2::Sdl) {
         self.frame += 1;
         let frame = self.frame;
