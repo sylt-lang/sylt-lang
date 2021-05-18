3052287bc3f88f37a4af549caa544ef1fbaf0ed9
Gustav Sörnäs <gustav@sornas.net>
2021-05-18 14:26:31 +0200

    add some comments to python

--- time_analysis.py
+++ time_analysis.py
@@ -13,7 +13,9 @@ if len(sys.argv) < 3:
 values = [[int(n) for n in open(file, "r").readlines()]
           for file in sys.argv[1:3]]
 
+# Get the mean/variance
 stats = list(map(scipy_stats.describe, values))
+# Calculate both with and without the assumption of equal variance
 p_eq_var = scipy_stats.ttest_ind(*values, equal_var=True).pvalue
 p_uneq_var = scipy_stats.ttest_ind(*values, equal_var=False).pvalue
 
@@ -25,6 +27,8 @@ print("Standard deviation")
 for stat in stats:
     print(f"{stat.variance**(1/2):.3f}")
 print()
+# Normal p-values are p<0.05 or even p<0.01 for statistical significance.
+# Perhaps try a baseline where you compare the programs with themselves.
 print("P values (small values -> reject equal mean hypothesis):")
 print("Equal variance   Unequal variance")
 print(f"{p_eq_var:.3f}            {p_uneq_var:.3f}")
