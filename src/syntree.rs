33b4ff0650ddd0863d17426a1404b246515487fe
Edvard Thörnros <edvard.thornros@gmail.com>
2021-05-30 20:11:05 +0200

    Parse a super simple expression

--- src/syntree.rs
+++ src/syntree.rs
@@ -1,7 +1,7 @@
-use std::path::PathBuf;
+use std::path::{PathBuf, Path};
 use std::collections::HashMap;
 use crate::error::Error;
-use crate::tokenizer::TokenStream;
+use crate::tokenizer::{TokenStream, Token};
 use super::Type as runtimeType;
 
 #[derive(Debug, Copy, Clone)]
@@ -189,6 +189,115 @@ pub struct Type {
     kind: TypeKind,
 }
 
-pub fn construct(tokens: TokenStream) -> Result<Prog, Vec<Error>> {
-    Err(Vec::new())
+type Tokens = [(Token, usize)];
+
+#[derive(Debug, Copy, Clone)]
+struct Context<'a> {
+    pub tokens: &'a Tokens,
+    pub curr: usize,
+    pub file: &'a Path,
+}
+
+impl<'a> Context<'a> {
+
+    fn new(tokens: &'a [(Token, usize)], file: &'a Path) -> Self {
+        Self { tokens, curr: 0, file, }
+    }
+
+    fn span(&self) -> Span {
+        Span { line: self.peek().1 }
+    }
+
+    fn line(&self) -> usize {
+        self.span().line
+    }
+
+    fn skip(&self, n: usize) -> Self {
+        let mut new = self.clone();
+        new.curr += n;
+        new
+    }
+
+    fn peek(&self) -> &(Token, usize) {
+        &self.tokens.get(self.curr).unwrap_or(&(Token::EOF, 0))
+    }
+
+    fn token(&self) -> &Token {
+        &self.peek().0
+    }
+
+    fn eat(&self) -> (&Token, Span, Context) {
+        (self.token(), self.span(), self.skip(1))
+    }
+}
+
+macro_rules! syntax_error {
+    ($ctx:expr, $( $msg:expr ),* ) => {
+        {
+            let msg = format!($( $msg ),*).into();
+            Error::SyntaxError {
+                file: $ctx.file.to_path_buf(),
+                line: $ctx.line(),
+                token: $ctx.token().clone(),
+                message: Some(msg),
+            }
+        }
+    };
+}
+
+macro_rules! raise_syntax_error {
+    ($ctx:expr, $( $msg:expr ),* ) => {
+        return Err(($ctx.skip(1), vec![syntax_error!($ctx, $( $msg ),*)]))
+    };
+}
+
+fn expression<'t>(ctx: Context<'t>) -> Result<(Context<'t>, Expression), (Context<'t>, Vec<Error>)> {
+    use Token as T;
+    use ExpressionKind::*;
+    let span = ctx.span();
+    Ok(match ctx.token() {
+        T::Float(v) => (ctx.skip(1), Expression { span, kind: Float(*v), }),
+        T::Int(v) => (ctx.skip(1), Expression { span, kind: Int(*v), }),
+        _ => {
+            raise_syntax_error!(ctx.skip(1), "Failed to parse expression");
+        }
+    })
+}
+
+fn outer_statement<'t>(ctx: Context<'t>) -> Result<(Context<'t>, Statement), (Context<'t>, Vec<Error>)> {
+    let span = ctx.span();
+    let (ctx, value) = expression(ctx)?;
+
+    use StatementKind::*;
+    Ok((ctx, Statement { span, kind: StatementExpression { value } }))
+}
+
+pub fn construct(tokens: &Tokens) -> Result<Module, Vec<Error>> {
+    let path = PathBuf::from("hello.sy");
+    let mut ctx = Context::new(tokens, &path);
+    let mut errors = Vec::new();
+    let mut statements = Vec::new();
+    while !matches!(ctx.token(), Token::EOF) {
+        if matches!(ctx.peek().0, Token::Newline) {
+            ctx = ctx.skip(1);
+            continue;
+        }
+        println!("A {:?}", ctx.peek());
+        ctx = match outer_statement(ctx) {
+            Ok((_ctx, statement)) => {
+                statements.push(statement);
+                _ctx
+            }
+            Err((_ctx, mut errs)) => {
+                errors.append(&mut errs);
+                _ctx
+            }
+        }
+    }
+
+    if errors.is_empty() {
+        Ok(Module { span: Span { line: 0 }, statements })
+    } else {
+        Err(errors)
+    }
 }
