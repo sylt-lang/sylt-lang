c2447d6316d476c1463ea8e5bb1b7205f7af6036
Gustav Sörnäs <gustav@sornas.net>
2021-06-22 22:19:04 +0200

    fix merge

--- sylt-typechecker/src/typechecker.rs
+++ sylt-typechecker/src/typechecker.rs
@@ -577,11 +577,18 @@ impl VM {
                     Type::Tuple(a) => {
                         self.push(a.get(slot as usize).cloned().unwrap_or(Type::Void));
                     }
-                    Type::Dict(k, v) if k.fits(&Type::Int) => {
+                    Type::Dict(k, v) if k.fits(&Type::Int, &prog.blobs).is_ok() => {
                         self.push((*v).clone());
                     }
-                    _ => {
+                    a => {
                         self.push(Type::Void);
+                        error!(
+                            self,
+                            RuntimeError::TypeError(op, vec![]),
+                            "Failed to index '{:?}' with '{:?}'",
+                            a,
+                            Type::Int
+                        );
                     }
                 }
             }
