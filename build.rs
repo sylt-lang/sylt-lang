585552c077914f25f6968bf407f681d197d81bc1
Edvard Thörnros <edvard.thornros@gmail.com>
2021-06-29 19:29:53 +0200

    build.rs got lost

---
+++ build.rs
@@ -0,0 +1,21 @@
+use std::path::Path;
+
+fn find_test_paths(directory: &Path) {
+    println!("cargo:rerun-if-changed={}", directory.display());
+    for entry in std::fs::read_dir(directory).unwrap() {
+        let path = entry.unwrap().path();
+        let file_name = path.file_name().unwrap().to_str().unwrap();
+
+        if file_name.starts_with("_") {
+            continue;
+        }
+
+        if path.is_dir() {
+            find_test_paths(&path);
+        }
+    }
+}
+
+fn main() {
+    find_test_paths(Path::new("../progs/"));
+}
