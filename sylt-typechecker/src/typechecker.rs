4a4ba223d55e30edc0e48116b721e8970d1b45c0
Gustav Sörnäs <gustav@sornas.net>
2021-08-10 20:46:04 +0200

    tuple changes (todo)

--- sylt-typechecker/src/typechecker.rs
+++ sylt-typechecker/src/typechecker.rs
@@ -927,6 +927,10 @@ mod op {
     use super::Type;
     use std::collections::HashSet;
 
+    fn tuple_dist_op(a: &Vec<Type>, n: &Type, f: fn(&Type, &Type) -> Type) -> Type {
+        Type::Tuple(a.iter().map(|a| f(a, n)).collect())
+    }
+
     fn tuple_bin_op(a: &Vec<Type>, b: &Vec<Type>, f: fn(&Type, &Type) -> Type) -> Type {
         Type::Tuple(a.iter().zip(b.iter()).map(|(a, b)| f(a, b)).collect())
     }
@@ -988,6 +992,8 @@ mod op {
             (Type::Int, Type::Int) => Type::Int,
             (Type::String, Type::String) => Type::String,
             (Type::Tuple(a), Type::Tuple(b)) if a.len() == b.len() => tuple_bin_op(a, b, add),
+            // TODO(ed): These need to be removed in more places.
+            // (Type::Tuple(t), n) | (n, Type::Tuple(t)) => tuple_dist_op(t, n, add),
             (Type::Unknown, a) | (a, Type::Unknown) if !matches!(a, Type::Unknown) => add(a, a),
             (Type::Unknown, Type::Unknown) => Type::Unknown,
             (Type::Union(a), b) | (b, Type::Union(a)) => union_bin_op(&a, b, add),
@@ -1003,7 +1009,10 @@ mod op {
         match (a, b) {
             (Type::Float, Type::Float) => Type::Float,
             (Type::Int, Type::Int) => Type::Int,
-            (Type::Tuple(a), Type::Tuple(b)) if a.len() == b.len() => tuple_bin_op(a, b, mul),
+            // TODO(ed): These need to be removed in more places.
+            // (Type::Tuple(a), Type::Tuple(b)) if a.len() == b.len() => tuple_bin_op(a, b, mul),
+            (Type::Tuple(t), n) | (n, Type::Tuple(t)) => tuple_dist_op(t, n, mul),
+
             (Type::Unknown, a) | (a, Type::Unknown) if !matches!(a, Type::Unknown) => mul(a, a),
             (Type::Unknown, Type::Unknown) => Type::Unknown,
             (Type::Union(a), b) | (b, Type::Union(a)) => union_bin_op(&a, b, mul),
@@ -1015,7 +1024,10 @@ mod op {
         match (a, b) {
             (Type::Float, Type::Float) => Type::Float,
             (Type::Int, Type::Int) => Type::Int,
-            (Type::Tuple(a), Type::Tuple(b)) if a.len() == b.len() => tuple_bin_op(a, b, div),
+            // TODO(ed): These need to be removed in more places.
+            // (Type::Tuple(a), Type::Tuple(b)) if a.len() == b.len() => tuple_bin_op(a, b, div),
+            (Type::Tuple(t), n) => tuple_dist_op(t, n, div),
+
             (Type::Unknown, a) | (a, Type::Unknown) if !matches!(a, Type::Unknown) => div(a, a),
             (Type::Unknown, Type::Unknown) => Type::Unknown,
             (Type::Union(a), b) | (b, Type::Union(a)) => union_bin_op(&a, b, div),
