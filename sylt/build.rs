06bcb02b2ea01cee75f4f6e286c823927dcb96a2
Gustav Sörnäs <gustav@sornas.net>
2021-08-14 01:22:46 +0200

    ignore errors in build.rs

--- sylt/build.rs
+++ sylt/build.rs
@@ -1,17 +1,19 @@
 use std::path::Path;
 
 fn find_test_paths(directory: &Path) {
-    println!("cargo:rerun-if-changed={}", directory.display());
-    for entry in std::fs::read_dir(directory).unwrap() {
-        let path = entry.unwrap().path();
-        let file_name = path.file_name().unwrap().to_str().unwrap();
+    if let Ok(entries) = std::fs::read_dir(directory) {
+        println!("cargo:rerun-if-changed={}", directory.display());
+        for entry in entries {
+            let path = entry.unwrap().path();
+            let file_name = path.file_name().unwrap().to_str().unwrap();
 
-        if file_name.starts_with("_") {
-            continue;
-        }
+            if file_name.starts_with("_") {
+                continue;
+            }
 
-        if path.is_dir() {
-            find_test_paths(&path);
+            if path.is_dir() {
+                find_test_paths(&path);
+            }
         }
     }
 }
