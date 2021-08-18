ef8e9ce3f52409a2ae767c4f5251048cba8f35dd
Edvard Thörnros <edvard.thornros@gmail.com>
2021-08-18 21:30:45 +0200

    Don't converty dummy values to strings

--- sylt-std/src/sylt.rs
+++ sylt-std/src/sylt.rs
@@ -378,6 +378,9 @@ sylt_macro::extern_function!(
 #[sylt_macro::sylt_doc(as_str, "Converts to a string representation", [_] Type::String)]
 #[sylt_macro::sylt_link(as_str, "sylt_std::sylt")]
 pub fn as_str<'t>(ctx: RuntimeContext) -> Result<Value, RuntimeError> {
+    if ctx.typecheck {
+        return Ok("".into());
+    }
     let values = ctx.machine.stack_from_base(ctx.stack_base);
     match values.as_ref() {
         [v] => Ok(Value::String(Rc::new(v.to_string()))),
