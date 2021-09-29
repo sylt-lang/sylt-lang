bd7b673d653ede13babe3f24b52dc7c04d53d120
Erik Mattfolk <erik.mattfolk@gmail.com>
2021-09-29 13:56:44 +0200

    Finish up rouge highlighter

--- docs/syntax-highlighter.rb
+++ docs/syntax-highlighter.rb
@@ -12,24 +12,23 @@ class Sylt < ::Rouge::RegexLexer
 
     rule %r(//.*?$), Comment::Single
 
-    rule %r(fn|do|end), Keyword
+    rule %r((and|or|not|if|else|loop|break|continue|blob|in|is|do|end|fn|ret|use)\b), Keyword
+    rule %r((bool|float|int|str|void)\b), Keyword::Type
+    rule %r((false|true|nil)\b), Keyword::Constant
 
     rule %r([A-Za-z_][A-Za-z0-9_]*), Name
 
+    rule %r((?i)(\d*\.\d+|\d+\.\d*)(e[+-]?\d+)?'), Num::Float
+    rule %r(\d+), Num::Integer
     rule %r("), Str::Double, :string
 
-    rule %r([\(\)]), Punctuation
+    rule %r([\(\)\[\]\{\},#]), Punctuation
 
-    rule %r(::), Operator
+    rule %r(\.|::|:=|:|=|->|'|\+|-|\*|\/|>|>=|<|<=|==|!=|\+=|-=|\/=|\*=|\?|\||!), Operator
   end
 
   state :string do
     rule %r("), Str::Double, :pop!
     rule %r([^"]+), Str::Double
-  end
-
-  state :comment do
-    rule %r($), Text, :pop!
-    rule %r([^$]), Comment::Single
   end
 end
