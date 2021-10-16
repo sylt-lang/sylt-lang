c6f73169b533459fdd2a4c0f134c87e556345cc4
Gustav Sörnäs <gustav@sornas.net>
2021-10-16 13:43:10 +0200

    add small comment

--- docs/navbar.py
+++ docs/navbar.py
@@ -3,4 +3,8 @@ import sys
 from bs4 import BeautifulSoup
 
 soup = BeautifulSoup("".join(sys.stdin.readlines()), "html.parser")
+
+# find <body>
+# insert <nav class="top-bar"> with <ul>-tags
+
 print(soup.prettify())
