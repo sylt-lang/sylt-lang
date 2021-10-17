2edc29a27f486abae90302a3ff9ce5e551e84a66
Gustav Sörnäs <gustav@sornas.net>
2021-10-17 09:07:50 +0200

    active navbar item

--- docs/navbar.py
+++ docs/navbar.py
@@ -1,17 +1,25 @@
+import os
 import sys
 
 from bs4 import BeautifulSoup
 
 LINKS = [("home", "index.html"), ("guide", "guide.html"), ("quick reference", "quick-reference.html")]
 
-soup = BeautifulSoup("".join(sys.stdin.readlines()), "html.parser")
+filename = sys.argv[1]
+with open(filename, "r") as f:
+    soup = BeautifulSoup("".join(f.readlines()), "html.parser")
 
-navbar = soup.new_tag("navbar")
+navbar = soup.new_tag("nav")
+navbar["class"] = "navbar"
 navbar_list = soup.new_tag("ul")
+navbar_list["class"] = "navbar-list"
 navbar.append(navbar_list)
 
 for text, href in LINKS:
     list_item = soup.new_tag("li")
+    list_item["class"] = "navbar-item"
+    if href == os.path.basename(filename):
+        list_item["class"] = "navbar-item navbar-item-active"
     link = soup.new_tag("a", href=href)
     link.string = text
     list_item.append(link)
