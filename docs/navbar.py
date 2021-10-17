f382b5a4d9e6c5a459288fe3da9e2d6ffc46b2c7
Gustav Sörnäs <gustav@sornas.net>
2021-10-17 08:53:28 +0200

    create and insert navbar ul

--- docs/navbar.py
+++ docs/navbar.py
@@ -2,9 +2,21 @@ import sys
 
 from bs4 import BeautifulSoup
 
+LINKS = [("home", "index.html"), ("guide", "guide.html"), ("quick reference", "quick-reference.html")]
+
 soup = BeautifulSoup("".join(sys.stdin.readlines()), "html.parser")
 
-# find <body>
-# insert <nav class="top-bar"> with <ul>-tags
+navbar = soup.new_tag("navbar")
+navbar_list = soup.new_tag("ul")
+navbar.append(navbar_list)
+
+for text, href in LINKS:
+    list_item = soup.new_tag("li")
+    link = soup.new_tag("a", href=href)
+    link.string = text
+    list_item.append(link)
+    navbar_list.append(list_item)
+
+soup.html.body.insert(0, navbar)
 
 print(soup.prettify())
