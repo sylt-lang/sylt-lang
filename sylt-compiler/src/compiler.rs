ee1a3e822f30791baf1c699fa78b2a0a7ee14ca6
Erik Mattfolk <erik.mattfolk@gmail.com>
2021-11-03 20:00:17 +0100

    Yeet some compiler warnings

--- sylt-compiler/src/compiler.rs
+++ sylt-compiler/src/compiler.rs
@@ -305,28 +305,7 @@ impl Compiler {
         let name = "/preamble/";
         let start_span = tree.modules[0].1.span;
         self.frames.push(Frame::new(name, start_span));
-        let _ctx = Context {
-            frame: 0,
-            ..Context::from_namespace(0)
-        };
-
         let num_constants = self.extract_globals(&tree);
-
-        // TODO
-        let num_functions = functions.len();
-        self.functions = functions
-            .to_vec()
-            .into_iter()
-            .enumerate()
-            .map(|(i, (s, f, sig))| (s.clone(), (i, f, parse_signature(&s, &sig))))
-            .collect();
-        assert_eq!(
-            num_functions,
-            self.functions.len(),
-            "There are {} names and {} extern functions - some extern functions share name",
-            self.functions.len(),
-            num_functions
-        );
 
         let statements = match dependency::initialization_order(&tree, &self) {
             Ok(statements) => statements,
