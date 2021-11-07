ec9bbb236ea25aa3b0afcf7f15cb50dd3cf6ea74
Edvard Thörnros <edvard.thornros@gmail.com>
2021-11-07 13:44:12 +0100

    Fix even more warnings

--- sylt-compiler/src/ty.rs
+++ sylt-compiler/src/ty.rs
@@ -1,5 +1,3 @@
-// TODO(ed, er): If you see these during code-review, remind us to remove it.
-#![allow(dead_code)]
 
 use std::collections::BTreeMap;
 
@@ -7,19 +5,20 @@ use std::collections::BTreeMap;
 pub enum Type {
     Unknown,
 
+    #[allow(dead_code)]
     Ty,
+    #[allow(dead_code)]
+    Invalid,
+
     Void,
     Int,
     Float,
     Bool,
     Str,
     Tuple(Vec<usize>),
-    // Union(BTreeSet<usize>),
     List(usize),
     Set(usize),
     Dict(usize, usize),
     Function(Vec<usize>, usize),
     Blob(String, BTreeMap<String, usize>),
-
-    Invalid,
 }
